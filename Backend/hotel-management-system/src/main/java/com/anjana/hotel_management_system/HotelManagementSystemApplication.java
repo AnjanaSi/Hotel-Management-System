c1f91ef277199adf9fa5f908f20eabbffe87ad0c
Chaga-Kariyawasam <chaganirmitha@gmail.com>
2025-07-13 07:38:25 +0530

    Fix: Updated configs and backend logic

--- Backend/hotel-management-system/src/main/java/com/anjana/hotel_management_system/HotelManagementSystemApplication.java
+++ Backend/hotel-management-system/src/main/java/com/anjana/hotel_management_system/HotelManagementSystemApplication.java
@@ -2,12 +2,17 @@ package com.anjana.hotel_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class HotelManagementSystemApplication {
 
 	public static void main(String[] args) {
+		String rawPassword = "Chaga@12";
+		String hashedPassword = "$2a$12$QK0WuTTBrDSOjKfJCvT5Je62ELexo2at0c6W64arJ2lv9lJGKzUNG";
 
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.matches(rawPassword, hashedPassword));
 		SpringApplication.run(HotelManagementSystemApplication.class, args);
 
 	}
